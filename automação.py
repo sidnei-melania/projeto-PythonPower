1307f0de7c140669f7f885773306e7064408da75
sidnei-melania <164648960+sidnei-melania@users.noreply.github.com>
2024-11-12 20:41:00 -0300

    Update automação.py

--- automação.py
+++ automação.py
@@ -3,20 +3,20 @@
 import pyautogui
 import time
 
-# pyautogui.write -> escrever um texto
+# pyautogui.write -> escrever texto
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.click -> clicar em algum lugar da tela
 # pyautogui.hotkey -> combinação de teclas
 pyautogui.PAUSE = 0.3
 
 # abrir o navegador (chrome, ou o que voce usa)
-pyautogui.press("win")
-pyautogui.write("chrome")
-pyautogui.press("enter")
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
 
 # entrar no link 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
-pyautogui.press("enter")
+pyautogui.press('enter')
 time.sleep(3)
 
 
@@ -24,7 +24,7 @@ time.sleep(3)
 # selecionar o campo de email
 pyautogui.click(x=685, y=451)
 # escrever o seu email
-pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.write('pythonimpressionador@gmail.com')
 pyautogui.press("tab") # passando pro próximo campo
 pyautogui.write("sua senha")
 pyautogui.click(x=955, y=638) # clique no botao de login
@@ -33,7 +33,7 @@ time.sleep(3)
 # Passo 3: Importar a base de produtos pra cadastrar
 import pandas as pd
 
-tabela = pd.read_csv("produtos.csv")
+tabela = pd.read_csv('produtos.csv')
 
 print(tabela)
 
